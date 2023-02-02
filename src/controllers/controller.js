a8f296b5b6f61006ae31e5a3bf64d462afc7c153
prianggarahm <priangga.rahmawanto@gmail.com>
2023-02-02 20:47:17 +0700

    fungsi untuk aboutMe

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,3 +1,12 @@
 exports.helloWorld = (req, res, next) => {
   res.send('<h1>Display Tested on expressjs</h1>');
 };
+
+// fungsi utk about-me
+exports.aboutMe = (req, res, next) => {
+  res.send({
+    Nama: 'Priangga R',
+    Alamat: 'Jakarta',
+    Pekerjaan: 'IT Officer',
+  });
+};
